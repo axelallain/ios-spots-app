a7b6dbc5ed69a200c48070f611622f0a4f06ea1e
Axel ALLAIN <axel.allain.a@gmail.com>
2023-12-22 00:37:45 +0100

    Initial Commit

---
+++ spots/spotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotsApp.swift
+//  spots
+//
+//  Created by Axel Allain on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct spotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
